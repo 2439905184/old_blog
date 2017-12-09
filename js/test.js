b8a6bfc17df5539255f1b740c45d1a66a8274029
2439905184 <29478722+2439905184@users.noreply.github.com>
2017-12-09 19:02:12 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,4 +1,4 @@
-function bgm(){
-    var bgm=document.getElementById("backgroundmusic")
-    bgm.play()
-}
+function (){var star=document.getElementById("fallen star")}
+function Seve(){
+    var seve=document.getLelmentById("Seve")
+    seve.play()}
