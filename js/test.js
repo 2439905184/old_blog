5e9dd492b4afc48e427bff2927cb840aafe06efd
2439905184 <29478722+2439905184@users.noreply.github.com>
2017-12-10 13:28:02 +0800

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
-function (){var star=document.getElementById("fallen star")}
-function seve(){
+
+seve=function (){
     var seve=document.getEelmentById("backgroundmusic")
     seve.src=/music/Time Lapse.mp3
     seve.play()}
